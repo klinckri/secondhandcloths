b5358432f2f834c85741205353b4e66385a2e715
Klincov Kristina (klinckri) <klinckri@students.zhaw.ch>
2022-11-12 13:36:10 +0100

    Initial commit with project setup

---
+++ src/main/java/ch/zhaw/secondhandcloths/SecondhandclothsApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.secondhandcloths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandclothsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondhandclothsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/secondhandcloths/SecondhandclothsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.secondhandcloths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandclothsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
